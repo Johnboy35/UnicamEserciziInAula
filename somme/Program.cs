afe28439b2f4139a99d67451d11038a9c800b83d
Moreno Gentili <brightsoul@tiscali.it>
2017-03-26 11:28:03 +0200

    Esercizio sulle somme

---
+++ somme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace somme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int risultato = 0;
+            while (true)
+            {
+                Console.WriteLine("Perfavore decidi cosa fare (scrivi stop per stoppare)");
+                string decisione = Console.ReadLine();
+                if (decisione == "stop")
+                {
+                    Console.WriteLine(risultato);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Per favore scrivi un numero");
+                    string primoNumero = Console.ReadLine();
+                    int variabileParse = int.Parse(primoNumero);
+                    risultato += variabileParse;
+                    Console.WriteLine(risultato);
+                }
+            }
+
+
+        }
+    }
+}
